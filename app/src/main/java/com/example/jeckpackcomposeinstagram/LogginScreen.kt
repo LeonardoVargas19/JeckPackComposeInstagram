f79341bf77cf7c86bd83c963638f1d6be9243299
leoromero <leonardo.vargas.ulsa@gmail.com>
2024-11-16 15:06:05 -0600

    Creacion del body de la pantalla

--- app/src/main/java/com/example/jeckpackcomposeinstagram/LogginScreen.kt
+++ app/src/main/java/com/example/jeckpackcomposeinstagram/LogginScreen.kt
@@ -1,17 +1,96 @@
 package com.example.jeckpackcomposeinstagram
 
 
+import android.app.Activity
+import android.text.Layout
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
-fun LogginScreen(){
-    Header()
+fun LogginScreen() {
+    Box(
+        Modifier
+            .fillMaxSize()
+            .padding(18.dp)
+    ) {
+        Header(Modifier.align(Alignment.TopEnd))
+        Body(Modifier.align(Alignment.Center))
+
+    }
 }
+
 @Composable
-fun Header(){
-    Icon(imageVector = Icons.Default.Close, contentDescription = null)
+fun Body(modifier: Modifier) {
+    val email = rememberSaveable { mutableStateOf("") }
+    val password = rememberSaveable { mutableStateOf("") }
+    Column(modifier = modifier) {
+        ImagesHeader()
+        Spacer(modifier = Modifier.size(16.dp))
+        Email(email.value) { email.value = it }
+        Spacer(modifier = Modifier.size(4.dp))
+        Password(password.value) { password.value = it }
+        Spacer(modifier = Modifier.size(8.dp))
+        ForgotPassword(Modifier.align(Alignment.End))
+
+    }
+}
+
+@Composable
+fun ForgotPassword(modifier: Modifier) {
+    Text(
+        text = "Forgot your password?",
+        fontSize = 12.sp,
+        fontWeight = FontWeight.Bold,
+        color = Color(0xff4EA8E9),
+        modifier = modifier
+    )
+}
+
+@Composable
+fun Password(password: String, onTextChanged: (String) -> Unit) {
+    TextField(value = password, onValueChange = { onTextChanged })
+}
+
+
+@Composable
+fun Email(email: String, onTextChanged: (String) -> Unit) {
+    TextField(value = email, onValueChange = { onTextChanged })
+
+}
+
+@Composable
+fun ImagesHeader() {
+    Image(painter = painterResource(id = R.drawable.insta), contentDescription = "Logo Instagram")
+}
+
+@Composable
+fun Header(modifier: Modifier) {
+    val activity = LocalContext.current as Activity
+    Icon(
+        imageVector = Icons.Default.Close,
+        contentDescription = "Close App",
+        modifier = modifier.clickable { activity.finish() })
 }
