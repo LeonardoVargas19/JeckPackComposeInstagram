60c1e3573ad1e1835f8c16853365f9741b68d0dc
leoromero <leonardo.vargas.ulsa@gmail.com>
2024-11-19 16:04:34 -0600

    Correccion de errores del proyecto

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,10 +37,12 @@ android {
     buildFeatures {
         compose = true
     }
+    buildToolsVersion = "34.0.0"
+
+
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -49,6 +51,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
     testImplementation(libs.junit)
 
     androidTestImplementation(libs.androidx.junit)
--- app/src/main/java/com/example/jeckpackcomposeinstagram/LogginScreen.kt
+++ app/src/main/java/com/example/jeckpackcomposeinstagram/LogginScreen.kt
@@ -15,15 +15,23 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Close
+
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -31,8 +39,13 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+
 
 @Composable
 fun LogginScreen() {
@@ -43,6 +56,41 @@ fun LogginScreen() {
     ) {
         Header(Modifier.align(Alignment.TopEnd))
         Body(Modifier.align(Alignment.Center))
+        Footer(
+            Modifier.align(Alignment.BottomCenter)
+        )
+    }
+}
+
+@Composable
+fun Footer(modifier: Modifier) {
+    Column(
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Divider(
+            Modifier
+                .background(Color.Gray)
+                .height(1.dp)
+                .fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.size(24.dp))
+        SignUp()
+        Spacer(modifier = Modifier.size(24.dp))
+    }
+}
+
+@Composable
+fun SignUp() {
+    Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+        Text(text = "Dont have an account?", fontSize = 12.sp, color = Color(0xFFB5B5B5))
+        Text(
+            text = "Sign Up.",
+            Modifier.padding(horizontal = 8.dp),
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFF4EA8E9)
+        )
+
     }
 }
 
@@ -76,7 +124,11 @@ fun SocialLogin() {
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.Center
     ) {
-        Image(painter = painterResource(id = R.drawable.fb), contentDescription = "Login Facebook", modifier = Modifier.size(16.dp))
+        Image(
+            painter = painterResource(id = R.drawable.fb_2),
+            contentDescription = "Login Facebook",
+            modifier = Modifier.size(16.dp)
+        )
         Text(
             text = "Continue as Rei Ayanami", fontSize = 14.sp, fontWeight = FontWeight.Bold,
             modifier = Modifier.padding(horizontal = 8.dp), color = Color(0xFF4EA8E9)
@@ -134,21 +186,67 @@ fun ForgotPassword(modifier: Modifier) {
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Password(password: String, onTextChanged: (String) -> Unit) {
+    val passwordVisibility = remember { mutableStateOf(false) }
+
+
+
     TextField(
         value = password,
         onValueChange = { onTextChanged },
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier.fillMaxWidth(),
+        placeholder = { Text(text = "Password") },
+        maxLines = 1,
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        trailingIcon = {
+            val imagen = if (passwordVisibility.value) {
+                Icons.Filled.AccountCircle
+            } else {
+                Icons.Filled.AccountCircle
+            }
+            IconButton(onClick = { passwordVisibility.value = !passwordVisibility.value }) {
+                Icon(imageVector = imagen, contentDescription = "show password")
+            }
+
+        },
+        visualTransformation = if (passwordVisibility.value) {
+            VisualTransformation.None
+        } else {
+            PasswordVisualTransformation()
+        },
+        colors = TextFieldDefaults.textFieldColors(
+            unfocusedIndicatorColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent,
+            containerColor = Color(0xFFFAFAFA),
+            cursorColor = Color(0xFFB2B2B2)
+        )
     )
 }
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Email(email: String, onTextChanged: (String) -> Unit) {
-    TextField(value = email, onValueChange = { onTextChanged }, modifier = Modifier.fillMaxWidth())
-
+    TextField(
+        value = email,
+        onValueChange = { newText -> onTextChanged(newText) },
+        modifier = Modifier.fillMaxWidth(),
+        placeholder = { Text(text = "Email") },
+        maxLines = 1,
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+        colors = TextFieldDefaults.textFieldColors(
+            unfocusedIndicatorColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent,
+            containerColor = Color(0xFFFAFAFA),
+            cursorColor = Color(0xFFB2B2B2)
+        )
+    )
 }
+
 
 @Composable
 fun ImagesHeader(modifier: Modifier) {
